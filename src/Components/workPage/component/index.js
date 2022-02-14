fcce819d24e2fcc75d3bd47d8a5b532e95eafe23
Arthur <newartroll11@gmail.com>
2022-02-14 10:31:18 +0400

    feat:( gsap curtain animation on hovorise)

--- src/Components/workPage/component/index.js
+++ src/Components/workPage/component/index.js
@@ -3,6 +3,27 @@ import { Plane, useCurtains } from "react-curtains";
 import gsap from "gsap";
 import { vertexShader, fragmentShader } from "../shaders/shaders";
 import "./index.css";
+import img1 from "../../../assets/images/work/mschristensen-august-krogh-hero.webp";
+import img2 from "../../../assets/images/work/mschristensen-baernholdt-hero.webp";
+import img3 from "../../../assets/images/work/mschristensen-bibelselskabet-hero.webp";
+import img4 from "../../../assets/images/work/mschristensen-bioinnovation-institute-hero.webp";
+import img5 from "../../../assets/images/work/mschristensen-dansk-markedsfoering-hero.webp";
+import img6 from "../../../assets/images/work/mschristensen-esmiley-hero.webp";
+import img7 from "../../../assets/images/work/mschristensen-firstborn-capital-hero.webp";
+import img8 from "../../../assets/images/work/mschristensen-fonda-hero.webp";
+import img9 from "../../../assets/images/work/mschristensen-foodwaste-hero.webp";
+import img10 from "../../../assets/images/work/mschristensen-haveselskabet-hero.webp";
+import img11 from "../../../assets/images/work/mschristensen-levendestreg-hero.webp";
+import img12 from "../../../assets/images/work/mschristensen-linkfactory-hero.webp";
+import img13 from "../../../assets/images/work/mschristensen-molio-hero.webp";
+import img14 from "../../../assets/images/work/mschristensen-novo-nordisk-foundation-hero.webp";
+import img15 from "../../../assets/images/work/mschristensen-pengi-hero.webp";
+import img16 from "../../../assets/images/work/mschristensen-pointvoucher-hero.webp";
+import img17 from "../../../assets/images/work/mschristensen-pointvoucher-studios-hero.webp";
+import img18 from "../../../assets/images/work/mschristensen-pointvoucher-studios-hero.webp";
+import img19 from "../../../assets/images/work/mschristensen-spotthespy-hero.webp";
+import img20 from "../../../assets/images/work/mschristensen-superbrugsen-hero.webp";
+import img21 from "../../../assets/images/work/mschristensen-westend-hero.webp";
 
 function Slideshow() {
   const [plane, setPlane] = useState(null);
@@ -15,6 +36,62 @@ function Slideshow() {
 
   const isChanging = useRef(false);
   const tween = useRef(null);
+
+  const [data, setData] = useState([
+    { name: "Bærnholdt", href: "#1", imgId: img1 },
+    { name: "BioInnovation Institute", href: "#2", imgId: img2 },
+    { name: "Levende Streg", href: "#3", imgId: img3 },
+    { name: "August Krogh", href: "#4", imgId: img4 },
+    { name: "ScienceNews", href: "#5", imgId: img5 },
+    { name: "Novo Nordisk Foundation", href: "#6", imgId: img6 },
+    { name: "Pointvouche", href: "#7", imgId: img7 },
+    { name: "Pointvoucher Studios", href: "#8", imgId: img8 },
+    { name: "FoodWaste", href: "#9", imgId: img9 },
+    { name: "eSmiley", href: "#10", imgId: img10 },
+    { name: "Spotthespy", href: "#11", imgId: img11 },
+    { name: "Fonda", href: "#12", imgId: img12 },
+    { name: "Bibelselskabet", href: "#13", imgId: img13 },
+    { name: "Molio", href: "#14", imgId: img14 },
+    { name: "Linkfactory", href: "#15", imgId: img15 },
+    { name: "Haveselskabet", href: "#16", imgId: img16 },
+    { name: "Firstborn Capital", href: "#17", imgId: img17 },
+    { name: "Pengi", href: "#18", imgId: img18 },
+    { name: "Dansk Markedsføring", href: "#19", imgId: img19 },
+    { name: "SuperBrugsen", href: "#20", imgId: img20 },
+    { name: "WestEnd London", href: "#21", imgId: img21 },
+  ]);
+
+  const [imageState, setImageState] = useState(img1);
+
+  const handleScroll = (e) => {
+    if (
+      e &&
+      e.target.scrollTop + e.target.offsetHeight + 1000 >=
+        e.target.scrollHeight &&
+      data.length <= 500
+    ) {
+      setData([...data, ...data]);
+    }
+    // window.innerHeight
+    if (e) {
+      const arr = document.querySelectorAll(".doesSelected");
+      arr.forEach((el) => {
+        if (
+          el.getBoundingClientRect().top < window.innerHeight / 2 + 50 &&
+          el.getBoundingClientRect().top > window.innerHeight / 2 - 100
+        ) {
+          el.style.color = "red";
+          setImageState(el.getAttribute("data-info"));
+          // console.log(el.getAttribute("data-info"));
+        } else {
+          el.style.color = "#999999";
+        }
+      });
+    }
+  };
+  useEffect(() => {
+    handleScroll();
+  }, []);
 
   useEffect(() => {
     if (slideshowInner.current) {
@@ -82,6 +159,15 @@ function Slideshow() {
     }
   };
 
+  // function MouseOver(event, data) {
+  //   event.target.parentNode.parentNode.style.boxShadow =
+  //     "inset 10000px -5000px 1000px 0px #000000";
+  //   event.target.parentNode.parentNode.style.boxShadow = "none";
+  //   event.target.parentNode.parentNode.style.transition =
+  //     "all 300ms ease-in-out";
+  //   setImageState(data);
+  // }
+
   useCurtains(
     (curtains) => {
       if (plane) {
@@ -116,19 +202,37 @@ function Slideshow() {
         // plane events
         onLoading={onLoading}
         onReady={onReady}
-        onClick={onClick}
+        onMouseMove={onClick}
       >
+        <div
+          className="work-page"
+          style={{
+            backgroundImage: plane && `url(${plane})`,
+          }}
+          ref={slideshowInner}
+        >
+          <nav
+            onScroll={(e) => handleScroll(e)}
+            id="element"
+            className="scroll"
+          >
+            {data &&
+              data.map((el, i) => (
+                <div
+                  className="this-item doesSelected"
+                  key={i}
+                  data-info={el.imgId}
+                  // onMouseMove={(event) => MouseOver(event, el.imgId)}
+                  // onMouseOut={(e) => MouseOut(e, el.imgId)}
+                >
+                  {el.name}
+                </div>
+              ))}
+          </nav>
+        </div>
         <div ref={slideshowInner}>
           <span>Click me !</span>
-          <img
-            src="https://www.curtainsjs.com/examples/medias/displacement.jpg"
-            data-sampler="displacement"
-            alt=""
-          />
-          <img src="https://unsplash.it/1920/1080?random=1" alt="" />
-          <img src="https://unsplash.it/1920/1080?random=2" alt="" />
-          <img src="https://unsplash.it/1920/1080?random=3" alt="" />
-          <img src="https://unsplash.it/1920/1080?random=4" alt="" />
+          {data && data.map((el, i) => <img src={el.imgId} alt="" />)}
         </div>
       </Plane>
     </>
